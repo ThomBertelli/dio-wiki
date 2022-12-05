6236a29b99707bb8398e5ac5dc2e01d5fcb5c792
Thomas Breno Bertelli Mendes <bertellimendes@gmail.com>
2022-12-04 23:13:58 -0300

    ADD remove function

---
+++ src/components/ItemRepo/styles.jsx
@@ -0,0 +1,33 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+    width: 80%;
+    
+
+    h3 {
+        font-size: 32px;
+        color: #FAFAFA;
+    }
+
+    p {
+        font-size:16px;
+        color: #FAFAFA60;
+        margin-bottom:20px;
+    }
+
+    a{
+        text-decoration:none;
+        color:#3388FF;
+    }
+
+    a.remover {
+        color: #FF0000;
+        margin-top:20px;
+        
+    }
+
+    hr {
+        color: #FAFAFA60;
+        margin: 20px 0;
+    }
+`
